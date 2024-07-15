fae6cd61388a91349199c92ebbe0d55002357347
Tikai7 <halimi.abdelkrim@outlook.fr>
2024-07-15 20:11:46 +0200

    fixed issues

--- src/model/SimCLR.py
+++ src/model/SimCLR.py
@@ -4,28 +4,30 @@ import torchvision.models as models
 
 
 class SimCLRBranch(nn.Module):
-    def __init__(self, feature_size=128) -> None:
+    def __init__(self, feature_size=128, use_context=False) -> None:
         super().__init__()
         assert feature_size <= 128, "[ERROR] Feature size has to be less than 128"
 
+        self.use_context = use_context
         self.RESNET_FEATURES_SIZE = 2048
         self.BERT_FEATURES_SIZE = 768
+        self.total_features = self.RESNET_FEATURES_SIZE + self.BERT_FEATURES_SIZE if use_context else self.RESNET_FEATURES_SIZE
 
         resnet = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
         resnet.fc = nn.Identity()
         self.feature_extractor = resnet    
         self.projection_head = nn.Sequential(
-            nn.Linear((self.RESNET_FEATURES_SIZE + self.BERT_FEATURES_SIZE) , 4 * feature_size),
+            nn.Linear(self.total_features , 4 * feature_size),
             nn.ReLU(),
             nn.Linear(4 * feature_size, feature_size)
         )
     
     def forward(self, X, C=None):
-        if C is  None:
+        if C is None and self.use_context:
             C = torch.zeros(X.shape[0], self.BERT_FEATURES_SIZE).to(X.device)
         
         H = self.feature_extractor(X)
-        H = torch.cat((H, C), dim=1)
+        H = torch.cat((H, C), dim=1) if self.use_context else H 
         Z = self.projection_head(H.flatten(start_dim=1))
         
         return H, Z
